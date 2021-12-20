ab57a36be25d472ef4e9aa3cd4482c991bd7b14d
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-12-20 14:37:57 -0500

    Create empty My Profile, configure linters and basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import logo from './img/rocket-logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+            and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Routes>
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import logo from '../img/rocket-logo.png'
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../img/rocket-logo.png';
 
-export const Header = () => {
-  return (
-    <nav>
-      <img src={ logo } alt="logo-img" />
-    </nav>
-  )
-}
+const Header = () => (
+  <Navbar bg="light" expand="lg">
+    <Container fluid>
+      <LinkContainer to="/">
+        <Navbar.Brand>
+          <img
+            src={logo}
+            alt="logo-img"
+            width="100"
+            height="100"
+          />
+        </Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link href="#home">Rockets</Nav.Link>
+          <Nav.Link href="#link">Missions</Nav.Link>
+          <LinkContainer to="/Profile"><Nav.Link>Profile</Nav.Link></LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+
+  </Navbar>
+);
+
+export default Header;
