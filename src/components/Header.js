7243bc8ab524e0e03fa924619edbccbbaa907808
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-12-20 15:03:43 -0500

    Add text to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,14 @@ const Header = () => (
   <Navbar bg="light" expand="lg">
     <Container fluid>
       <LinkContainer to="/">
-        <Navbar.Brand>
+        <Navbar.Brand className="d-flex align-items-center">
           <img
             src={logo}
             alt="logo-img"
             width="100"
             height="100"
           />
+          <h1 className="mx-4"> Space Travelers&apos; Hub </h1>
         </Navbar.Brand>
       </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
