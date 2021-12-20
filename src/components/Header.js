4ca2538f1bcfba80a8ba6d7a747905f67b52d63c
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-20 18:21:01 +0200

    Add image logo and header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../img/rocket-logo.png'
+
+export const Header = () => {
+  return (
+    <nav>
+      <img src={ logo } alt="logo-img" />
+    </nav>
+  )
+}
