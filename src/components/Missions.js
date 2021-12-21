8975699e378bcd335a082be0d6da9c9a9b6af72f
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-21 18:45:46 +0200

    Add useEffect for single render

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import {useSelector, useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchMissions } from '../redux/missions/missions'
 
 export const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!missions.length) dispaatch(fetchMissions())
+  }, [])
   return (
     <div>
       <p>work </p>
