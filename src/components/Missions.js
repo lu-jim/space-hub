34a7b72cca30dd266ced3b07a20d2c4532afdf08
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-21 19:43:08 +0200

    Add mission setup and fix errors

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -3,23 +3,22 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchMissions } from '../redux/missions/missions'
 
 export const Missions = () => {
-  const missions = useSelector((state) => state.missions);
-  const dispatch = useDispatch()
+   const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!missions.length) dispatch(fetchMissions())
-  }, [])
-
+    if (!missions.length) dispatch(fetchMissions());
+  }, []);
 
   return (
-    <main className='main'>
-      <table className='missions-table'>
+    <main className="main">
+      <table className="missions-table">
         <thead>
           <tr>
-            <th>Missions</th>
-            <th>Descriptions</th>
-            <th>Status</th>
-            <th>Join</th>
+            <th>Mission</th>
+            <th>Description</th>
+            <th className="status-col">Status</th>
+            <th className="join-col">Join</th>
           </tr>
         </thead>
         <tbody>
@@ -27,12 +26,12 @@ export const Missions = () => {
             const colorClass = index % 2 === 0 ? 'grey-bg' : 'white-bg';
             return (
               <tr key={mission.mission_id} className={colorClass}>
-                <th className='mission-title'>{mission.mission_name}</th>
+                <th className="mission-title">{mission.mission_name}</th>
                 <td>{mission.description}</td>
                 <td className="mission-status">Not a member</td>
                 <td className="mission-join">Join Mission</td>
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
