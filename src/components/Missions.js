db378d3cd28bdc2b268fac30b30a0af7254006ad
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-22 12:21:59 +0200

    Fix linter errors

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMissions } from '../redux/missions/missions';
-import './Missions.css'
+import './Missions.css';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
