0e995310f025491613dd0d87cc99c21723fa8ddb
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-22 11:31:11 +0200

    Finish implementation of missions

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
+// import { LinkContainer } from 'react-router-bootstrap';
 import { NavLink } from 'react-router-dom';
 import logo from '../img/rocket-logo.png';
 
 const Header = () => (
   <Navbar bg="light" expand="lg">
     <Container fluid>
-      <LinkContainer to="/">
+      <NavLink to="/">
         <Navbar.Brand className="d-flex align-items-center">
           <img
             src={logo}
@@ -19,7 +19,7 @@ const Header = () => (
           />
           <h1 className="mx-4"> Space Travelers&apos; Hub </h1>
         </Navbar.Brand>
-      </LinkContainer>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import store from './redux/configureStore';
 
 ReactDOM.render(
   <>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
-
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>,
   document.getElementById('root'),
 );
