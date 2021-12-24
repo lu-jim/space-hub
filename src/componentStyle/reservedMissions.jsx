443674a9cafa4ca6788b33214315024356d138a3
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-24 10:14:03 +0200

    Add changes

--- src/componentStyle/reservedMissions.jsx
+++ src/componentStyle/reservedMissions.jsx
@@ -3,5 +3,11 @@ import { useSelector } from "react-redux";
 
 const ReservedMissions = () => {
   const missions = useSelector((state) => state.missions);
-  const reservedMissions = missions.filter((mission) =>missions.reserved)
+  const reservedMissions = missions.filter((mission) => missions.reserved)
+  
+  return (
+    <section>
+      <h2 className="filtered-list">My Missions</h2>
+    </section>
+  )
 }
