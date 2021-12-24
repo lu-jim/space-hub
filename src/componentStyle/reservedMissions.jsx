11bdc27be0342cc8755f08dcab3c9052314cfea4
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-24 11:31:06 +0200

    Fix linter errors

--- src/componentStyle/reservedMissions.jsx
+++ src/componentStyle/reservedMissions.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import Button from './Button.style'
 import Link from "./Link.style";
 import { toggleMissionReservation } from '../redux/missions/missions'
