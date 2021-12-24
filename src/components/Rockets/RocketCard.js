96eb637dbfdeac0ef7a1decffd5ee448c06e734c
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-12-24 09:33:56 -0500

    Implement booking feature and show booking status on the page

--- src/components/Rockets/RocketCard.js
+++ src/components/Rockets/RocketCard.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
+import BookButton from './BookButton';
 import { bookRocket } from '../../redux/rockets/rockets';
 
 const RocketCard = (props) => {
   const {
-    id, description, rocketName, flickrImages,
+    rocketId, description, rocketName, flickrImages, isBooked,
   } = props;
+
   const dispatch = useDispatch();
   const reserveRocket = (rocket) => {
     dispatch(bookRocket(rocket));
   };
 
   return (
-    <div className="row p-3 m-0" id={id}>
+    <div className="row p-3 m-0" id={rocketId}>
       <div className="col-6 col-lg-4">
         <img src={flickrImages[0]} alt="completed" className="img-fluid" />
       </div>
       <div className="col-6 col-lg-8 p-2">
         <h2>{rocketName}</h2>
         <p className="p-4">{description}</p>
-        <Button variant="primary" onClick={() => reserveRocket(id)}>
-          Reserve Rocket
-        </Button>
+        <BookButton
+          isBooked={isBooked}
+          rocketId={rocketId}
+          onClickEvent={(rocketId) => reserveRocket(rocketId)}
+
+        />
         {' '}
       </div>
     </div>
@@ -31,10 +35,15 @@ const RocketCard = (props) => {
 };
 
 RocketCard.propTypes = {
-  id: PropTypes.number.isRequired,
+  rocketId: PropTypes.number.isRequired,
   rocketName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   flickrImages: PropTypes.arrayOf(PropTypes.string).isRequired,
+  isBooked: PropTypes.bool,
+};
+
+RocketCard.defaultProps = {
+  isBooked: false,
 };
 
 export default RocketCard;
