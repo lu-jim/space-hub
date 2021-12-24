2adf0f479f6ae3269c1c16698e6b81a29370a097
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-24 10:28:29 +0200

    Add reseeved mission render part

--- src/componentStyle/reservedMissions.jsx
+++ src/componentStyle/reservedMissions.jsx
@@ -3,28 +3,38 @@ import { useSelector } from "react-redux";
 
 const ReservedMissions = () => {
   const missions = useSelector((state) => state.missions);
-  const reservedMissions = missions.filter((mission) => missions.reserved)
-  
+  const reservedMissions = missions.filter((mission) => mission.reserved);
+  const dispatch = useDispatch();
+
+  const handleMissionReservation = (id) => {
+    dispatch(toggleMissionReservation(id));
+  };
+
   return (
     <section className="filtered-list">
       <h2 className="filtered-list__title">My Missions</h2>
       <ul className="filtered-list__list">
-        {!!reservedMissions && reservedMissions.map((mission) => (
-          <li key={mission.mission_id}>
-            <h3 className="filtered-list__list__item__title">
-              {mission.mission_name}
-            </h3>
-          </li>
-        ))}
+        {!!reservedMissions
+          && reservedMissions.map((mission) => (
+            <li className="filtered-list__list__item" key={mission.mission_id}>
+              <h3 className="filtered-list__list__item__title">
+                {mission.mission_name}
+              </h3>
+              <Link to={mission.link} />
+              <Button
+                isMissionJoined={mission.reserved}
+                onClick={() => handleMissionReservation(mission.mission_id)}
+              />
+            </li>
+          ))}
         {!reservedMissions.length && (
           <p className="filtered-list__list__item">
-            No reserved missions
+            There are no reserved missions
           </p>
         )}
-
       </ul>
     </section>
-  )
-}
+  );
+};
 
 export default ReservedMissions
